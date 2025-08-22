b293a3645f76df7cc21978aa9750cdfad2bf3fdb
Shashikant Pandey <pandeyshashikant1434@gmail.com>
2025-08-23 03:58:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [Text, setText] = useState("");
+  const [Qr, setQr] = useState("");
+
+  const QrGenerator =(Text)=>{
+    if (Text=="") {
+      document.getElementById("input").classList.add("error");
+      setTimeout(() => {
+        document.getElementById("input").classList.remove("error");
+      }, 1000);
+    }
+
+    (Text)?(setQr("https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+Text)):(setQr(""))
+  }
+  return (
+    <div className="h-screen flex flex-col justify-center items-center bg-gradient-to-r
+from-[#fda4af]
+to-[#f43f5e]">
+      <div className="space-y-4 bg-gradient-to-r
+from-[#0f172a]
+to-[#334155] py-8 px-18 rounded-lg">
+        <h1 className="text-2xl font-bold">QR Code Generator</h1>
+        <fieldset className="fieldset">
+          <legend className="fieldset-legend">Enter Text or URL</legend>
+          <input id="input"
+          type="text" value={Text} onChange={(e)=>setText(e.target.value)} className="input w-54 bg-white focus:outline-none focus:border-2 text-gray-700" placeholder="Type here" />
+        </fieldset>
+        {Qr && <img src={Qr} alt="QR Code" className="mx-auto p-2 bg-white rounded-lg" />}
+        <button onClick={()=>(QrGenerator(Text))} className="btn btn-dash btn-success min-w-54">Generate QR</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
